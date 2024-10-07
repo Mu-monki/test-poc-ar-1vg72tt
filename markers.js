6069a464f612e9f252af01a74a81f5e91336a062
Francis Miguel <zfmggarciajr@917ventures.com>
2024-10-07 10:53:30 +0800

    event listener

--- markers.js
+++ markers.js
@@ -3,11 +3,12 @@ console.log('JS File Loaded!')
 const sakunwari_marker = document.getElementById('marker-klimabukasan'); // Replace with the ID of your marker entity
 const sakunwari_audio = document.getElementById('sakunwari-audio');
 
-marker.addEventListener('markerFound', () => {
+sakunwari_marker.addEventListener('markerFound', () => {
+    mar
     sakunwari_audio.play();
 });
 
 
-marker.addEventListener('markerLost', () => {
+sakunwari_marker.addEventListener('markerLost', () => {
     sakunwari_audio.pause();
 });
