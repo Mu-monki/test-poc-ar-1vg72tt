7a652a8138977d70068916909b5271e05019646e
Francis Miguel <zfmggarciajr@917ventures.com>
2024-10-07 11:12:56 +0800

    audio script udpate

--- markers.js
+++ markers.js
@@ -1,15 +1,15 @@
 console.log('JS File Loaded!')
 
-const sakunwari_marker = document.getElementById('marker-klimabukasan'); // Replace with the ID of your marker entity
-const sakunwari_audio = document.getElementById('sakunwari-audio');
+// const sakunwari_marker = document.getElementById('marker-klimabukasan'); // Replace with the ID of your marker entity
+// const sakunwari_audio = document.getElementById('sakunwari-audio');
 
-sakunwari_marker.addEventListener('markerFound', () => {
-    console.log('MARKER FOUND!')
-    sakunwari_audio.play();
-});
+// sakunwari_marker.addEventListener('markerFound', () => {
+//     console.log('MARKER FOUND!')
+//     sakunwari_audio.play();
+// });
 
 
-sakunwari_marker.addEventListener('markerLost', () => {
-    console.log('MARKER LOST!')
-    sakunwari_audio.pause();
-});
+// sakunwari_marker.addEventListener('markerLost', () => {
+//     console.log('MARKER LOST!')
+//     sakunwari_audio.pause();
+// });
