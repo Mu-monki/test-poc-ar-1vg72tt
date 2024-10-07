36b0a7c1f478d568e5483378ed7db7c574f69e75
Francis Miguel <zfmggarciajr@917ventures.com>
2024-10-07 10:49:27 +0800

    scrited audio behavior

--- markers.js
+++ markers.js
@@ -1,1 +1,13 @@
 console.log('JS File Loaded!')
+
+const sakunwari_marker = document.getElementById('marker-klimabukasan'); // Replace with the ID of your marker entity
+const sakunwari_audio = document.getElementById('sakunwari-audio');
+
+marker.addEventListener('markerFound', () => {
+    sakunwari_audio.play();
+});
+
+
+marker.addEventListener('markerLost', () => {
+    sakunwari_audio.pause();
+});
