00f48c2f852432bf9570235dad6c04854a6e9e05
Francis Miguel <zfmggarciajr@917ventures.com>
2024-10-14 21:23:05 +0800

    changed tap event to tocuhstart

--- markers.js
+++ markers.js
@@ -208,7 +208,7 @@ AFRAME.registerComponent('arrowhandler', {
             console.log('Updating Card...');
         });
 
-        klimaCard.addEventListener('tap', function(e) {
+        klimaCard.addEventListener('touchstart', function(e) {
             console.log('tap event', e)
             console.log('Card tapped!');
             updateKlimabukasanCard();
